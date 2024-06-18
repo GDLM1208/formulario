a1408e46276b5473260de7050c44ee93cbab5788
Gabriel Llerena <gllerena1208@gmail.com>
2024-06-17 22:35:02 -0500

    Segunda build

--- .netlify/functions-internal/ssr/chunks/Envio_BQsLn8-U.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/Envio_DWe20JCb.mjs').then(n => n.E);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CakZOSx7.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BTdt0B3B.mjs');
-
-export { page };
